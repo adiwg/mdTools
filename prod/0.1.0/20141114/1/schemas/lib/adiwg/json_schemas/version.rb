fbfc70849ee7618e2cd5a4edb56644fd665fe3bf
Josh Bradley <jbradley@arcticlcc.org>
2014-12-05 15:13:05 -0900

    Remove and gitignore build files.

--- prod/0.1.0/20141114/1/schemas/lib/adiwg/json_schemas/version.rb
+++
@@ -1,6 +0,0 @@
-module ADIWG
-    module JsonSchemas
-        # Current schema version number
-        VERSION = "0.9.0"
-    end
-end
--- prod/0.1.0/20141204/1/schemas/lib/adiwg/json_schemas/version.rb
+++
@@ -1,6 +0,0 @@
-module ADIWG
-    module JsonSchemas
-        # Current schema version number
-        VERSION = "0.9.4"
-    end
-end
